5400f0197f7b2cac6a9cf5add1713a3f62860e31
Jir Wong <jirwong@gmail.com>
2026-08-05 20:33:21 +1000

    chore: use @eslint/js core rules and update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import js from '@eslint/js';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
 
@@ -14,22 +13,16 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: './tsconfig.json',
       },
       globals: {
         console: 'readonly',
         process: 'readonly',
       },
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
     rules: {
       ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
       ...prettierConfig.rules,
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
+      'no-unused-vars': [
         'warn',
         {
           argsIgnorePattern: '^_',
